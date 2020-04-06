b38bf181c201aabaff1141cd84d445420864a3b8
Atul Kumar <atulkumar458@gmail.com>
2020-04-06 21:02:17 +0530

    initial commit

---
+++ src/main/java/com/atul/j8stream/j8stream/J8streamApplication.java
@@ -0,0 +1,13 @@
+package com.atul.j8stream.j8stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J8streamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J8streamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atul/j8stream/j8stream/J8streamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atul.j8stream.j8stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J8streamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
